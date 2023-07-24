556ce2330cb3bdcc6a8d408d153d1af21d39c94d
Matheus Vidal <matheusvidalpro@gmail.com>
2023-07-24 17:13:44 -0400

    feat: faz o header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,10 +1,40 @@
-import logo from '../assets/logo.png';
+import { useState } from "react";
+import logo from "../assets/logo.png";
 
 function Header() {
+  const [isActive, setIsActive] = useState(0);
+
+  const toggleMenu = (value: number) => {
+    setIsActive(value);
+  };
+
   return (
-    <header>
-      <img src={ logo } alt="" />
-      <h1 className="text-white font-paradox font-bold text-5xl">Tchez</h1>
+    <header className="flex items-center justify-between p-4 pt-4fixed top-0 w-full z-50">
+      <img src={logo} alt="Logo Tchez" className="w-28" />
+      <ul className="flex space-x-8">
+        <li>
+          <a
+            href="#"
+            className={`text-slate-100 text-2xl ${
+              isActive === 1 ? "border-b-2" : ""
+            }`}
+            onClick={() => toggleMenu(1)}
+          >
+            Sobre
+          </a>
+        </li>
+        <li>
+          <a
+            href="#"
+            className={`text-slate-100 text-2xl ${
+              isActive === 2 ? "border-b-2" : ""
+            }`}
+            onClick={() => toggleMenu(2)}
+          >
+            Contato
+          </a>
+        </li>
+      </ul>
     </header>
   );
 }
