4cc86e5f55a69a630290e1afecaa10816045caeb
Matheus Vidal <matheusvidalpro@gmail.com>
2023-07-31 22:48:16 -0400

    fix props.children

--- src/components/Bar.tsx
+++ src/components/Bar.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-function Bar(props: { children: string | number | boolean | React.ReactElement<any, string | React.JSXElementConstructor<any>> | Iterable<React.ReactNode> | React.ReactPortal | null | undefined }) {
+function Bar(props: { children: string | number | boolean | React.ReactElement<unknown, string | React.JSXElementConstructor<unknown>> | Iterable<React.ReactNode> | React.ReactPortal | null | undefined }) {
   return (
     <div className="flex justify-center items-center bg-white w-full h-10 gap-3">
       {props.children}
