735088296f0f22c168ec32fb1fc85069e51d243e
raphaelbaere <raphaelbaere@id.uff.br>
2023-08-01 22:40:49 -0300

    some fixes

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 function Header() {
 
   return (
-    <header className="flex items-center justify-between p-20 pb-4 pt-4 sticky top-0 w-full bg-black">
+    <header className="flex items-center justify-between p-20 z-50 pb-4 pt-4 sticky top-0 w-full bg-black">
       <Link to="/">
         <img src={logo} alt="Logo Tchez" className="md:w-28 w-36 md:-ml-0 -ml-14" />
       </Link>
--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -7,7 +7,7 @@ function About() {
       <Header />
       <div className="flex justify-center items-center flex-col gap-10 w-full">
       <h1 className="text-zinc-200 text-2xl font-custom mt-10">Sobre o Artista</h1>
-        <div className="flex justify-center items-center flex-col gap-10 md:w-2/4 sm:w-2/4">
+        <div className="flex justify-center items-center flex-col gap-10 md:w-2/4 sm:w-2/4 w-3/4">
         <p className="text-zinc-200 text-justify font-custom">Sempre influenciado pela música, Tchez iniciou sua carreira em 2018 como DJ e, em 2021, embarcou na jornada de produtor musical.</p>
         <p className="text-zinc-200 text-justify font-custom">Com uma abordagem versátil, ele rapidamente se destacou por sua habilidade em se adaptar a diferentes públicos, navegando entre o techno, minimal techno e progressive house, criando assim sua própria identidade sonora.</p>
         <p className="text-zinc-200 text-justify font-custom">Seu primeiro EP, intitulado "Lunar" (Future Techno), alcançou a impressionante posição de #7 no Top 100 Techno Releases, recebendo apoio de renomados artistas como Bultech, Marco Carola e Richie Hawtin</p>
