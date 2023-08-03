9a6d2dc5ace92cc7cce49357970472994643dbce
Matheus Vidal <matheusvidalpro@gmail.com>
2023-08-03 15:16:30 -0400

    fix: fixed contact on mobile

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -5,9 +5,11 @@ function Contact() {
   return (
     <div>
       <Header />
-      <main className="m-28">
-        <h1 className="text-zinc-200 text-4xl font-custom mb-10">Contato</h1>
-        <address className="text-zinc-200 text-xl text-justify font-custom">
+      <main className="m-5 flex flex-col items-center justify-center md:m-28">
+        <h1 className="text-zinc-200 text-2xl mb-10 md:text-4xl font-custom md:mb-10">
+          Contato
+        </h1>
+        <address className="text-zinc-200 m-5 text-lg md:text-xl text-justify font-custom mb-44">
           Email:{" "}
           <a href="mailto:tchezpastorello@gmail.com">
             tchezpastorello@gmail.com
