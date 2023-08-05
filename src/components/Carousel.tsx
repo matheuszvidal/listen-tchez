64ad2b5397ed500a49a0348a4dfb2d99d3ef2285
Matheus Vidal <matheusvidalpro@gmail.com>
2023-08-04 21:18:43 -0400

    fix: correção do tamanho do carrossel

--- src/components/Carousel.tsx
+++ src/components/Carousel.tsx
@@ -4,10 +4,9 @@ import Carousel1 from '../assets/Carousel1.jpg'
 import Carousel2 from '../assets/Carousel2.jpg'
 import Carousel3 from '../assets/Carousel3.jpg'
 
-
 function Carousel() {
   return (
-    <div className=" w-full h-96 flex justify-center items-center p-10 bg-zinc-200 shadow-xl">
+    <div className=" w-full h-auto flex justify-center items-center p-10 pt-5 bg-zinc-200 shadow-xl">
       <AwesomeSlider className="h-5/6 max-w-screen-lg">
         <div data-src={Carousel1} />
         <div data-src={Carousel2} />
